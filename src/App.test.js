45f001579bd0c1d21e40f8eda11295c30a3ea8f6
hlee2542 <ou40a9rtbp@gmail.com>
2020-02-13 13:39:58 -0800

    added import mutationobserver-shim to test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,14 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+import 'mutationobserver-shim';
+
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test('first name* is on form', async () => {
+  const {getByText} = render(<App />);
+  const firstName = getByText(/first name*/i);
+  expect(firstName).toBeInTheDocument();
 });
