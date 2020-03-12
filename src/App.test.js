dd8a2aa5f79d39acf261dc757cfc4a0e1dbc6ea8
MrR3set <bairon.pazrobles@gmail.com>
2020-03-12 16:47:42 -0700

    idk what im adoing

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,14 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("checks for the first name", ()=>{
+  const {getByText} = render(<App />);
+  const header = getByText(/First Name/i);
+  expect(header).toBeInTheDocument();
+});
+test("checks for the last name", ()=>{
+  const {getByText} = render(<App />);
+  const header = getByText(/lasst Name/i);
+  expect(header).toBeInTheDocument();
+});
