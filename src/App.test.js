70595c005a9d6fc2c169d4f24987dac9653468e3
emma <sonicjane2020@gmail.com>
2020-03-12 15:49:02 -0700

    Console logged to test app.text.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import * as rtl from '@testing-library/react';
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+  console.log("hello");
 });
