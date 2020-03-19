36e766de4cf0ebc8042da356e75156109b6a9792
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-03-19 12:01:39 -0700

    File Added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const ContactForm = () => {
   const [data, setData] = useState();
   const { register, errors, handleSubmit, reset } = useForm({
-    mode: "onBlur"
+    mode: 'onBlur'
   });
   const onSubmit = data => {
     setData(data);
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name*</label>
+          <label htmlFor='firstName'>First Name*</label>
           <input
-            name="firstName"
-            placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            name='firstName'
+            placeholder='bill'
+            ref={register({ required: true, maxLength: 6 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -26,10 +26,10 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="lastName">Last Name*</label>
+          <label htmlFor='lastName'>Last Name*</label>
           <input
-            name="lastName"
-            placeholder="luo"
+            name='lastName'
+            placeholder='luo'
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -38,24 +38,24 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor='email' placeholder='bluebill1049@hotmail.com'>
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name='email' ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
-          <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <label htmlFor='message'>Message</label>
+          <textarea name='message' ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre style={{ textAlign: 'left', color: 'white' }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input type='submit' />
       </form>
     </div>
   );
