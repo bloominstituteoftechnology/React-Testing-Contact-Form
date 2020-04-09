83d8c5f79f41de3a035191ac4c09353d876a9415
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-09 16:14:38 -0500

    Uncommented other code to test, did not pass though.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -15,30 +15,30 @@ test('Make sure the inputs are visible.', () => {
     getByLabelText(/message/i);
 });
 
-// test('Adding items to the contact form and Submitting the form.', () => {
-//     const { getByLabelText, getByText, getByTestId } = render(<ContactForm />);
+test('Adding items to the contact form and Submitting the form.', () => {
+    const { getByLabelText, getByText, getByTestId } = render(<ContactForm />);
 
-//     const fNameInput = getByLabelText(/firstName/i);
-//     const lNameInput = getByLabelText(/lastName/i);
-//     const email = getByLabelText(/email/i);
-//     const message = getByLabelText(/message/i);
+    const fNameInput = getByLabelText(/first Name/i);
+    const lNameInput = getByLabelText(/last Name/i);
+    const email = getByLabelText(/email/i);
+    const message = getByLabelText(/message/i);
 
-//     fireEvent.change(fNameInput, { target: { value: 'MyFirstName' } });
-//     fireEvent.change(lNameInput, { target: { value: 'MyLastName' } });
-//     fireEvent.change(email, { target: { value: 'MyEmail@email.com' } });
-//     fireEvent.change(message, {
-//         target: {
-//             value:
-//                 'Gotta type in a fake message dj dfsjak fnasd fe fv er vs dg rtg gbv tyyj hbfg rytjh grfv btrehgv ty jhtg.',
-//         },
-//     });
+    fireEvent.change(fNameInput, { target: { value: 'MyFirstName' } });
+    fireEvent.change(lNameInput, { target: { value: 'MyLastName' } });
+    fireEvent.change(email, { target: { value: 'MyEmail@email.com' } });
+    fireEvent.change(message, {
+        target: {
+            value:
+                'Gotta type in a fake message dj dfsjak fnasd fe fv er vs dg rtg gbv tyyj hbfg rytjh grfv btrehgv ty jhtg.',
+        },
+    });
 
-//     expect(fNameInput.value).toBe('MyFirstName');
-//     expect(lNameInput.value).toBe('MyLastName');
-//     expect(email.value).toBe('MyEmail@email.com');
-//     expect(message.value).toBe(
-//         'Gotta type in a fake message dj dfsjak fnasd fe fv er vs dg rtg gbv tyyj hbfg rytjh grfv btrehgv ty jhtg.'
-//     );
+    expect(fNameInput.value).toBe('MyFirstName');
+    expect(lNameInput.value).toBe('MyLastName');
+    expect(email.value).toBe('MyEmail@email.com');
+    expect(message.value).toBe(
+        'Gotta type in a fake message dj dfsjak fnasd fe fv er vs dg rtg gbv tyyj hbfg rytjh grfv btrehgv ty jhtg.'
+    );
 
-//     fireEvent.click(getByText(/submit/i));
-// });
+    fireEvent.click(getByText(/submit/i));
+});
