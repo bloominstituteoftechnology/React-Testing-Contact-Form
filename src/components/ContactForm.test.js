9f8c400f5797a2f5e5fc0fbd08f6b8ef2e80bd89
Golden Eagle <joowoon.kang@biola.edu>
2020-04-09 14:14:50 -0700

    tested inserting values on inputs

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render,fireEvent} from '@testing-library/react';
 import ContactForm from './ContactForm';
 
-test("test this", () => {
-    const { getByLabelText, getByText, getByTestId } = render(<ContactForm />);
+test("render a component!", () => {
+    const result = render(<ContactForm />);
+    // console.log(result)
+})
 
-    const firstName = getByLabelText(/First Name*/i);
+test("test the label text of first name", () => {
+    const { getByLabelText, getByTestId } = render(<ContactForm />);
+
+    const firstName = getByLabelText(/First/);
     expect(firstName).toBeInTheDocument();
 
+    const firstNameId = getByTestId("firstName");
+    expect(firstNameId).toBeInTheDocument();
 });
+
+test('form submit adds new info to the list', () => {
+    
+  });
+
+test("test to sure that the inputs can be filled in", () => {
+    const {getByLabelText} = render(<ContactForm />)
+
+    const firstNameInput = getByLabelText(/First/i);
+    const lastNameInput = getByLabelText(/Last/i);
+    const emailInput = getByLabelText(/Email/i);
+    const messageInput = getByLabelText(/Message/i);
+
+    fireEvent.change(firstNameInput, {target: {value:"hue"}});
+    fireEvent.change(lastNameInput, {target: {value:"will"}});
+    fireEvent.change(emailInput, {target: {value:"email@mail.com"}})
+    fireEvent.change(messageInput, {target: {value:"gretting"}})
+    
+   expect(firstNameInput.value).toBe("hue")
+})
