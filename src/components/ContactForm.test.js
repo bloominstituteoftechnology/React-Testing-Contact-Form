d2ad4d690cae1fc7b761960d3b34a462dd74392a
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-09 15:26:13 -0500

    Added a contactform.test. Testing that ContactForm renders

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from '@testing-library/react';
+import ContactForm from './ContactForm'
+
+test('ContactForm Renders', ()=> {
+    render(<ContactForm/>)
+}
