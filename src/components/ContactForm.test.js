abc905b7a87239858e8f671763839d7afc4b9edb
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-09 16:52:03 -0500

    Added the fix to act().

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -17,14 +17,20 @@ test('Make sure the inputs are visible.', () => {
     getByLabelText(/message/i);
 });
 
-test('Adding items to the contact form and Submitting the form.', () => {
+test('Adding items to the contact form and Submitting the form.', async () => {
+    //Starting off the tests with these.
     const { getByLabelText, getByText } = render(<ContactForm />);
+    //Had an error about act(), looked up a solution
+    const promise = Promise.resolve();
+    const handleUpdateUsername = jest.fn(() => promise);
 
+    //Retrieving each different form
     const fNameInput = getByLabelText(/first Name/i);
     const lNameInput = getByLabelText(/last Name/i);
     const email = getByLabelText(/email/i);
     const message = getByLabelText(/message/i);
 
+    //Type in information for each input
     fireEvent.change(fNameInput, { target: { value: 'MyFirstName' } });
     fireEvent.change(lNameInput, { target: { value: 'MyLastName' } });
     fireEvent.change(email, { target: { value: 'MyEmail@email.com' } });
@@ -35,12 +41,14 @@ test('Adding items to the contact form and Submitting the form.', () => {
         },
     });
 
+    //Should output this bit of info
     expect(fNameInput.value).toBe('MyFirstName');
     expect(lNameInput.value).toBe('MyLastName');
     expect(email.value).toBe('MyEmail@email.com');
     expect(message.value).toBe(
         'Gotta type in a fake message dj dfsjak fnasd fe fv er vs dg rtg gbv tyyj hbfg rytjh grfv btrehgv ty jhtg.'
     );
-
+    //Click on submit
     fireEvent.click(getByText(/submit/i));
+    await act(() => promise);
 });
