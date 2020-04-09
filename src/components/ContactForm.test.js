48699654298025b7d9a110bccf628526dca9672a
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-09 15:40:25 -0500

    Checked that ContactForm renders, and now checking the input fields.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { render } from '@testing-library/react';
-import ContactForm from './ContactForm'
+import ContactForm from './ContactForm';
 
-test('ContactForm Renders', ()=> {
-    render(<ContactForm/>)
-}
+test('ContactForm Renders', () => {
+    render(<ContactForm />);
+});
+
+test('Adding items to the contact form and Submitting the form.', () => {
+    const(getByLabelText, getByText, getByTestId) = render(<ContactForm />)
+    
+
+    const fNameInput = getLabelByText(/firstName/i)
+    const lNameInput = getLabelByText(/lastName/i)
+    const email = getLabelByText(/email/i)
+    const message = getLabelByText(/message/i)
+
+    
+})
