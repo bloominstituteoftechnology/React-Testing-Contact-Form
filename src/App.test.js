00c588a2c627ec9c142ac3ba027d502767131c15
Altyn Berdiyeva <aberdiyeva@lisaacademy.org>
2020-04-22 17:23:19 -0500

    some testing is here, the last one is not passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
-  render(<App />);
+	render(<App />);
 });
+
+test("gets by testid", () => {
+	const { getByTestId } = render(<App />);
+	expect(getByTestId(/app/i)).toBeInTheDocument();
+});
+
+test("input with placeholder 'bill' is in the dom", () => {
+	const { getByPlaceholderText } = render(<App />); //arrange
+	const lastNameInput = getByPlaceholderText("luo"); //act
+	expect(lastNameInput).toBeTruthy(); //assert
+	expect(lastNameInput).toBeInTheDocument();
+});
+
+
+test("submit button", () => {
+  const {getByText} = render(<ContactForm/>);
+  const submitButton = getByText("submit");
+  expect(submitButton).toBeInTheDocument();
+})
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,7 +17,7 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
-            placeholder="bill"
+            placeholder="bill" 
             ref={register({ required: true, maxLength: 3 })}
           />
           {errors.firstName && (
@@ -29,7 +29,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
-            placeholder="luo"
+            placeholder="luo" //testing
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -55,7 +55,8 @@ const ContactForm = () => {
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        {/* testing */}
+        <input type="submit" /> 
       </form>
     </div>
   );
