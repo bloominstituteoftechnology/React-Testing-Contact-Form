70cac73dc0a12b0fc8a7557d851fdc6b19e949f6
Cody Branson <codyboi7000@gmail.com>
2020-05-07 15:58:31 -0600

    updated the code to fix the testing error i've been getting all day.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,76 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
-test("renders App without crashing", () => {
-  render(<App />);
+test("finds input First Name, Last Name, Email and Message without crashing", () => {
+  const { getByLabelText, getByText, findAllByText } = render(<App />);
+
+  const firstName = getByText(/first name/i);
+  const lastName = getByText(/last name/i);
+  const email = getByText(/email/i);
+  const message = getByText(/message/i);
+
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+  expect(message).toBeInTheDocument();
+
+  // fireEvent.change(firstName, {
+  //   target: { name: "First Name", value: "Edd" }
+  // });
 });
+
+test("this test allows users to fill out forms", () => {
+  const { getByLabelText } = render(<ContactForm />);
+
+  fireEvent.change(getByLabelText(/first name/i), {
+    target: { value: "code" }
+  });
+  fireEvent.change(getByLabelText(/last name/i), {
+    target: { value: "codecode" }
+  });
+  fireEvent.change(getByLabelText(/email/i), {
+    target: { value: "code@code.com" }
+  });
+  fireEvent.change(getByLabelText(/message/i), {
+    target: { value: "this is just another test message" }
+  });
+});
+
+//
+// test("form fills in fields and submits", () => {
+//   const { getByLabelText, getByText, findAllByText } = render(<ContactForm />);
+//
+//   // query for the form inputs
+//   const fnInput = getByLabelText(/firstName/i);
+//   const lnInput = getByLabelText(/lastName/i);
+//   const emailInput = getByLabelText(/email/i);
+//   const messageInput = getByLabelText(/message/i);
+//
+//   // fireEvent function from RTL to fill in the inputs
+//   fireEvent.change(fnInput, {
+//     target: { name: "firstName", value: "aa" }
+//   });
+//   fireEvent.change(lnInput, {
+//     target: { name: "lastName", value: "codecode" }
+//   });
+//   fireEvent.change(emailInput, {
+//     target: { name: "email", value: "code@code.com" }
+//   });
+//   fireEvent.change(messageInput, {
+//     target: { name: "message", value: "this is the message" }
+//   });
+
+// console.log(speciesInput.value);
+
+// query for the submit button
+// const submitButton = getByText(/submit!/i);
+
+// clicking the button
+// fireEvent.click(submitButton);
+// fireEvent.click(submitButton);
+
+// assertion
+// findAllByText(/elephant/i);
+// });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,9 +4,9 @@ import { useForm } from "react-hook-form";
 const ContactForm = () => {
   const [data, setData] = useState();
   const { register, errors, handleSubmit } = useForm({
-    mode: "onBlur",
+    mode: "onBlur"
   });
-  const onSubmit = (data) => {
+  const onSubmit = data => {
     setData(data);
   };
 
@@ -16,6 +16,8 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
+            type="text"
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +30,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,14 +44,18 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            id="message"
+            name="message"
+            ref={register({ required: false })}
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
