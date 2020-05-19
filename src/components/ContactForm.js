ae729bdc628f12861c1f52922212347209e2f5ac
lex-marie790 <lexi.marie790@gmail.com>
2020-05-18 18:30:21 -0700

    mvp complete

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -38,10 +38,10 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email" placeholder="bluebill1049@hotmail.com" >
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" type="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
