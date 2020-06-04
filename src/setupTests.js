6ac4dbdb5595a4609c412ab7acdc8b722148fe2d
jasonelliots <j.elliotschwartz@gmail.com>
2020-06-04 14:25:33 -0700

    finishedmvp

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,38 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ContactForm from "./components/ContactForm";
+
+test("renders correctly with first name", () => {
+  render(<ContactForm />);
+
+  const firstNameInput = screen.getByTestId(/firstName/i)
+  const lastNameInput = screen.getByTestId(/lastName/i)
+  const emailInput = screen.getByTestId(/email/i)
+  const messageInput = screen.getByTestId(/message/i)
+
+
+  fireEvent.change(firstNameInput, {
+    target: {value: "Jason" }
+  });
+  fireEvent.change(lastNameInput, {
+    target: {value: "Schwartz" }
+  });
+  fireEvent.change(emailInput, {
+    target: {value: "Jason@jason.com" }
+  });
+  fireEvent.change(messageInput, {
+    target: {value: "Hello!" }
+  });
+
+// const submitButton = screen.getByTestId(/submit/i);
+
+// fireEvent.click(submitButton);
+
+// const newEntry = screen.findByText(/jason/i)
+// expect(newEntry).toBeInTheDocument(); 
+
+});
