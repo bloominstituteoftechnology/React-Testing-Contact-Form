cdb263d8fedf9770223369589dd737a27e414277
Katherine-Kumar <Katherine2bk@gmail.com>
2020-06-11 16:41:13 -0700

    added a test for rendering contactform, trying to understand why getByLabelText is not working

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,11 +2,13 @@ import React from "react";
 import ContactForm from "./ContactForm";
 import {render, fireEvent} from "@testing-library/react";
 
+
+test("renders form", () => {
+    render(<ContactForm />)
+});
+
 test("first and last name, email and message are being rendered", () => {
-    const { getByLabelText } = render(<ContactForm />);
+    const { getByLabelText } = render(<ContactForm />)
     getByLabelText(/first Name/i);
-    getByLabelText(/last Name/i);
-    getByLabelText(/email/i);
-    getByLabelText(/message/i);
 });
 
