a56dac43f43a4d6009e2473ca486a14639b402fd
JenVest2020 <jenvest2019@gmail.com>
2020-07-16 15:09:58 -0500

    reading docs for react-use-form testing/maxlength

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+// import { App } from './App';
+import ContactForm from './components/ContactForm';
 
-test("renders App without crashing", () => {
-  render(<App />);
-});
+// test("renders App without crashing", () => {
+//   const { getByText } = render(<App />);
+
+//   console.log(getByText);
+// });
+
+test('first name input max length 10', () => {
+  const container = render(<ContactForm />);
+
+  // console.log(container);
+
+  const firstnameInput = container.queryByPlaceholderText(/edd/i);
+
+  fireEvent.input(firstnameInput, { target: { value: '0123465789' } });
+
+  expect(firstnameInput.maxlength).toBe(10);
+  expect(firstnameInput.value).toBe('0123456789')
+
+
+
+})
 
