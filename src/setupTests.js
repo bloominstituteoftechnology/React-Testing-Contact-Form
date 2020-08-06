d4089e36e4d2a42278e17d8ec637312b3a3f09c3
Parker williams <parkster246@gmail.com>
2020-08-06 14:20:43 -0500

    running tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,31 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+
+
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ContactForm from "./components/ContactForm";
+
+test("renders correctly with first name", () => {
+  render(<ContactForm />);
+
+  const firstNameInput = screen.getByTestId(/firstName/i)
+  const lastNameInput = screen.getByTestId(/lastName/i)
+  const emailInput = screen.getByTestId(/email/i)
+  const messageInput = screen.getByTestId(/message/i)
+
+
+  fireEvent.change(firstNameInput, {
+    target: {value: "Parker" }
+  });
+  fireEvent.change(lastNameInput, {
+    target: {value: "Williams" }
+  });
+  fireEvent.change(emailInput, {
+    target: {value: "parker@parker.com" }
+  });
+  fireEvent.change(messageInput, {
+    target: {value: "Suh dude!" }
+  })
+})
