c7b9cbfd7491ba75c8b5de1bbcc488a6bc8eed59
unknown <olem2233@gmail.com>
2020-08-06 21:23:09 -0700

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
-});
+ test("renders App without err", () => {
+   render(<App />);
+ });
