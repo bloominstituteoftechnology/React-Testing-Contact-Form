a1af69f22fffed08db3050fbd4318ef48a916f5e
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-13 20:39:12 -0500

    added testid to all inputs in ContactForm.js, conducted firstName and lastName tests in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,15 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test('First name is rendering', () => {
+  const { getByTestId } = render(<App />);
+  const firstName = getByTestId('firstName-input');
+  expect(firstName.textContent).toBe("");
+});
+
+test('Last name is rendering', () => {
+  const { getByTestId } = render(<App />);
+  const lastName = getByTestId('lastName-input');
+  expect(lastName.textContent).toBe("");
+});
