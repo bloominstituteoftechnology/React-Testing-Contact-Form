532ae9586effb5365ee12921d5e7bd0617159c5d
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-13 20:46:58 -0500

    created tests for rendering the text content of the email input and the message text area

--- src/App.test.js
+++ src/App.test.js
@@ -6,14 +6,26 @@ test("renders App without crashing", () => {
   render(<App />);
 });
 
-test('First name is rendering', () => {
+test('first name is rendering', () => {
   const { getByTestId } = render(<App />);
   const firstName = getByTestId('firstName-input');
   expect(firstName.textContent).toBe("");
 });
 
-test('Last name is rendering', () => {
+test('last name is rendering', () => {
   const { getByTestId } = render(<App />);
   const lastName = getByTestId('lastName-input');
   expect(lastName.textContent).toBe("");
+});
+
+test('email is rendering', () => {
+  const { getByTestId } = render(<App />);
+  const email = getByTestId('email-input');
+  expect(email.textContent).toBe("");
+});
+
+test('message textarea is rendering', () => {
+  const { getByTestId } = render(<App />);
+  const message = getByTestId('message-textarea');
+  expect(message.textContent).toBe("");
 });
