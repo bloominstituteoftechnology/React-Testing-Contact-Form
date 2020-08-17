78c3b6cbc8cf27fb6464efff465fb8023daea2bf
unknown <Katherine2bk@gmail.com>
2020-08-17 15:55:16 -0700

    tried redoing project over weekend, tests passes, but still one fails... submit button works, inputs display on screen

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,29 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ContactForm from "./ContactForm";
-import {render, fireEvent, screen, waitFor, findByText} from "@testing-library/react";
+import {render, fireEvent} from "@testing-library/react";
 
 // test 1, if contact form renders
 
 
-test("renders form", () => {
-    render(<ContactForm />)
-});
+// test("renders form", () => {
+//     render(<ContactForm />)
+// });
 
-it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<ContactForm />, div)
-})
+// it("renders without crashing", () => {
+//     const div = document.createElement("div");
+//     ReactDOM.render(<ContactForm />, div)
+// })
 
 // test 2, if labels are being rendered
-
-test("first and last name, email and message are being rendered", () => {
-    const { getByLabelText } = render(<ContactForm />)
-     getByLabelText(/first name/i);
-    getByLabelText(/last Name/i);
-     getByLabelText(/email/i);
-     getByLabelText(/message/i);
-});
 
 // to see if there's a bug...
 
@@ -39,29 +31,82 @@ test("first and last name, email and message are being rendered", () => {
 //     const nameinput = await findByText(/first name/i)
 // })
 
-test('form submit adds new items', () => {
-    const { getByLabelText } = render(<ContactForm />)
+// test("button works", () => {
+//     const { getByTestId } = render(<ContactForm />);
+//     const button = getByTestId("submit");
 
-    const firstNameInput = getByLabelText(/first Name/i);
-    const lastNameInput = getByLabelText(/last Name/i);
-    const emailInput = getByLabelText(/email/i);
-    const messageInput = getByLabelText(/message/i);
+//     fireEvent.click(button);
+//     expect(button).toBeVisible();
+// })
 
-    fireEvent.change(firstNameInput, { target: { value: 'testing firstName'} })
-    fireEvent.change(lastNameInput, { target: { value: 'testing lastName'} })
-    fireEvent.change(emailInput, { target: { value: 'testing email'} })
-    fireEvent.change(messageInput, { target: { value: 'testing message'} })
 
-    expect(firstNameInput.value).toBe('testing firstName');
-    expect(lastNameInput.value).toBe('testing lastName');
-    expect(emailInput.value).toBe('testing email');
-    expect(messageInput.value).toBe('testing message');
+// test("renders labels above each ContactForm input", () => {
+//     const { getByLabelText } = render(<ContactForm />);
+  
+//     const firstNameLabel = getByLabelText(/first name/i)
+//     const lastNameLabel = getByLabelText(/last name/i)
+//     const emailLabel = getByLabelText(/email/i)
+//     const messageLabel = getByLabelText(/message/i)
+  
+//     expect(firstNameLabel).toBeInTheDocument();
+//     expect(lastNameLabel).toBeInTheDocument();
+//     expect(emailLabel).toBeInTheDocument();
+//     expect(messageLabel).toBeInTheDocument();
+  
+//   });
+  
+//   test("inputs on ContactForm are registering", () => {
+//     const { getByLabelText } = render(<ContactForm />);
+  
+//     const firstNameInput = getByLabelText(/first name/i)
+//     const lastNameInput = getByLabelText(/last name/i)
+//     const emailInput = getByLabelText(/email/i)
+//     const messageInput = getByLabelText(/message/i)
+  
+//     fireEvent.change( firstNameInput, {target: {value: "test first name"}})
+//     fireEvent.change( lastNameInput, {target: {value:  "test last name"}})
+//     fireEvent.change( emailInput, {target: {value: "testing@email.com"}})
+//     fireEvent.change( messageInput, {target: {value: "test message"}})
+  
+//     expect(firstNameInput.value).toBe("testing first name")
+//     expect(lastNameInput.value).toBe("testing last name")
+//     expect(emailInput.value).toBe("testing@email.com")
+//     expect(messageInput.value).toBe("testing message")
+//   })
+
+//   
+
+test("Form renders", () => {
+render(<ContactForm />);
+
+ const firstNameInput = screen.getByTestId(/firstname/i)
+ const lastNameInput = screen.getByTestId(/lastname/i)
+ const emailInput = screen.getByTestId(/email/i)
+ const messageInput = screen.getByTestId(/message/i)
+ const submitButton = screen.getByTestId(/submit/i)
+
+ fireEvent.change(firstNameInput, {target: {value: "Kat"}});
+ fireEvent.change(lastNameInput, {target: {value: "Kumar"}});
+ fireEvent.change(emailInput, {target: { value:"email@email.com"}});
+ fireEvent.change(messageInput, {target: {value: "testing message"}});
+ fireEvent.click(submitButton);
+
+ expect(firstNameInput.value).toBe("Kat")
+ expect(lastNameInput.value).toBe("Kumar")
+ expect(emailInput.value).toBe("email@email.com")
+ expect(messageInput.value).toBe("testing message")
 })
 
-test("button works", () => {
-    const { getByTestId } = render(<ContactForm />);
-    const button = getByTestId("submit");
+test("Errors show up if value is empty", () => {
+    render(<ContactForm />)
 
-    fireEvent.click(button);
-    expect(button).toBeVisible();
-})
+    const submitButton =screen.getByTestId(/submit/i)
+
+    fireEvent.click(submitButton);
+
+    setTimeout(() => {
+        expect(screen.getByTestId(/firstNameError/i)).toBeInTheDocument();
+        expect(screen.getByTestId(/lastNameError/i)).toBeInTheDocument();
+        expect(screen.getByTestId(/emailError/i)).toBeInTheDocument();
+    }, 1);
+});
