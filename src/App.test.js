d9d20ef6d66fc1b38fee8ca8be0934dc2209c6fe
Daniel Terry <daniel.terry2121@gmail.com>
2020-09-10 23:22:24 -0400

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,73 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByLabelText } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
+import { act } from "react-dom/test-utils";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+/*
+describe("On Submit works"), () => {
+  it('calls onSubmit function', () => {
+    const mockOnSubmit = jest.fn()
+    const { getByLabelText, getByRole } = render(<ContactForm onSubmit = {mockOnSubmit} />)
+    
+    fireEvent.change(getByLabelText("email"), {target: {value: "email.test@lambda.com"}})
+    fireEvent.change(getByLabelText("firstName"), {target: {value: "Daniel"}})
+    fireEvent.change(getByLabelText("lastName"), {target: {value: "Terry"}})
+   
+      fireEvent.click(getbyRole("button"))
+    expect(mockOnSubmit).toHaveBeenCalled()
+  })
+}*//*
+test("Submit" , () => {
+
+})
+*/
+/*
+describe("Valid First Name"), () => {
+  it('accepts firstname entry', () => { const firstNameInput = getByLabelText("firstName")
+  fireEvent.change(firstNameInput, {target:{value:"Daniel"}})
+  expect(firstNameInput).toHaveValue("Daniel")
+  })
+}*/
+test('Have firstName' , () => {
+  const { getByTestId } = render(<ContactForm />)
+  const firstNameInput = getByTestId("Edd")
+  fireEvent.change(firstNameInput, {target:{value:"Daniel"}})
+  expect(firstNameInput).toHaveValue("Daniel")
+})
+/*
+describe("Valid Last Name"), () => {
+}
+*/
+test('Have Last Name ',  () => {
+  const { getByTestId } = render(<ContactForm />)
+  const lastNameInput = getByTestId("Burke")
+  fireEvent.change(lastNameInput , {target: {value:"Terry"}})
+  expect(lastNameInput).toHaveValue("Terry")
+})
+test('Have Valid Email' , () => {
+  const { getByTestId } = render(<ContactForm />)
+  const emailInput = getByTestId("email")
+  fireEvent.change(emailInput, {target:{value:'email@123.com'}})
+  expect(emailInput).toHaveValue('email@123.com')
+})
+/*
+describe("Valid Email"), () => {
+  const onSubmit = jest.fn()
+  const data = {firstName : 'Maksym', lastName: 'Petrusenko', email:'DanielTerry@gmail.com', message : 'wazzup bitchess'}
+
+    fireEvent.change(getByLabelText(/firstName/i), {target:{value:user.firstName}})
+
+    fireEvent.change(utils.getByLabelText(/lastName/i), {target:{value:user.lastName}})
+    
+    fireEvent.change(utils.getByLabelText(/email/i), {target:{value:user.email}})
+
+    fireEvent.click(getByText(/submit/i))
+
+    expect(onSubmit).toHaveBeenCalled(1)
+    expect(onSubmit).toHaveBeenCalledWith(data)
+}
+*/
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,8 +17,9 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
+            data-testid="Edd"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -29,6 +30,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
+            data-testid= "Burke"
             placeholder="Burke"
             ref={register({ required: true })}
           />
@@ -41,7 +43,7 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" data-testid= "email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
