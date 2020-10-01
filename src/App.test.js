c3af258301c4e0b4072bb7878010e509ffa6446b
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-10-01 19:12:40 -0400

    mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,52 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByTestId } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
+
+
+
+
+test(" form renders without errors", () => {
+  render(<ContactForm />);
+})
+
+test("all input fields are working", async () => {
+
+const { getByLabelText } = render(<ContactForm />);
+
+const firstNameInput = getByLabelText(/first name*/i);
+
+expect(firstName).toBeTruthy();
+
+const lastNameInput = getByLabelText(/last name*/i);
+
+expect(lastName).toBeTruthy();
+
+const emailInput = getByLabelText(/Email/i);
+
+expect(email).toBeTruthy();
+
+const message = getByLabelText(/message*/i);
+
+expect(message).toBeTruthy();
+
+fireEvent.change(firstNameInput,{target: {name:'firstName', value:'Battle'}
+});
+fireEvent.change(lastNameInput,{target: {name:'lastName', value:'Cat'}
+});
+fireEvent.change(emailInput,{target: {name:'email', value:'skeletor@greystonemanner.com'}
+});
+
+}) 
+
+
+
+
+
+
+
+
+
 
 test("renders App without crashing", () => {
   render(<App />);
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,8 +17,9 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
-            placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            id="firstName"
+            placeholder="Alex"
+            ref={register({ required: true, minLength: 3 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -29,6 +30,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
+            id="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
           />
@@ -38,24 +40,39 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email"
+            placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+
+          <input
+            name="email"
+            id="email"
+            ref={register({ required: true })} />
+
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
+
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+
+          <textarea
+            name="message"
+            id="message"
+            ref={register({ required: false })} />
+
         </div>
+
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+
+        <input id="submit" type="submit" />
+
       </form>
     </div>
   );
