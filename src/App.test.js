7604535a5f66320f3504b6fa06e00e13e097900d
Christopher Barrett <municipalshock@gmail.com>
2020-10-01 17:07:49 -0700

    test input labels

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,45 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("renders h1 with no errors", () => {
+  //Arrange
+  render(<App />);
+
+  //Act
+  const header = screen.getByText("Add User!");
+
+  console.log(header);
+
+  //Assert
+  expect(header).toBeInTheDocument();
+});
+
+describe("Tests Contact Form", () => {
+  test("user can fill out and submit form", () => {
+    //Arrange
+    render(<ContactForm />);
+    //Act
+    //query each input
+    const firstNameInput = screen.getByLabelText(/firstName/i); // Breaks here//
+    const lasNameInput = screen.getByLabelText(/lastName/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const messageInput = screen.getByLabelText(/message/i);
+    const button = screen.getByRole("button", { name: /submit/i });
+
+    // fireEvent.change(firstNameInput, {target:{name:'firstName', value: ''}})
+    // fireEvent.change(lastNameInput, {target:{name:'lastName', value: ''}})
+    // fireEvent.change(emailInput, {target:{name:'email', value: ''}})
+    // fireEvent.change(messageInput, {target:{name:'message', value: ''}})
+
+    //fill out form
+    //click
+    //Assert
+    //test that our for input exists
+  });
 });
