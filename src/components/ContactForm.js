4e6b6cb96308c46c0cf3f0f7fbe1046a8fc38e24
unknown <tompsherman@gmail.com>
2020-10-01 20:50:03 -0400

    successful 'happy path' form tested

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,6 +16,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +29,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -38,17 +40,24 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
-            Email*
-          </label>
-          <input name="email" ref={register({ required: true })} />
+          <label htmlFor="email">Email*</label>
+          <input 
+            id="email"
+            name="email" 
+            placeholder="bluebill1049@hotmail.com"
+            ref={register({ required: true })}
+            />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea 
+            id="message"
+            name="message" 
+            placeholder="enter message (optional)"
+            ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,4 +6,25 @@ describe("test contact form", ()=>{
     test("app renders without error", ()=>{
         render(<ContactForm/>)
     })
+    test("user can fill out and submit form", async ()=>{
+        render(<ContactForm/>)
+
+        const firstNameInput = screen.getByLabelText(/first name/i)
+        const lastNameInput = screen.getByLabelText(/last name/i)
+        const emailInput = screen.getByLabelText(/email/i)
+        const messageInput = screen.getByLabelText(/message/i)
+
+
+        const submitButton = screen.getByRole(/button/i)
+        
+        fireEvent.change(firstNameInput, {target: { value: "Tom"}})
+        fireEvent.change(lastNameInput, {target: { value: "Sherman"}})
+        fireEvent.change(emailInput, {target: { value: "tom@tom.tom"}})
+        fireEvent.change(messageInput, {target: { value: "tomtomtom"}})
+
+
+        fireEvent.click(submitButton)
+
+        await screen.findByText(/Tom/i)
+    })
 })
