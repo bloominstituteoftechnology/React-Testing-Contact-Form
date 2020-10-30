122fb978b85f0d26b652506a8c98f138bae3089f
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2020-10-29 20:46:31 -0400

    mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+
 });
+
+test('can text in name input', () => {
+  //Arrange: render component
+  render(<App />)
+
+  //Act:
+  //1. Get access
+  const name = screen.getByLabelText(/first name*/i)
+
+  //2. add text to our fields
+  fireEvent.change(name, {target: {value: 'Ben', name:'boy'}});
+})
