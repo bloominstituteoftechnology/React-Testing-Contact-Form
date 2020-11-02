1dd78a16b1c5e379c747b225d7bf33a0af5ecba9
Ismail Al Kamal <ismail@nawatt.com>
2020-11-02 23:14:19 +0300

    MVP done

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,112 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("Check Firstname empty field", async () => {
+  render(<App />);
+
+  const firstName = screen.getByTestId("firstName");
+
+  await act(async () => {
+    fireEvent.change(firstName, { target: { value: "" } });
+    fireEvent.blur(firstName);
+  });
+
+  expect(firstName).toBeInTheDocument();
+  expect(firstName).toHaveValue("");
+  expect(screen.queryByTestId("firstNameError")).toBeInTheDocument();
+});
+
+test("Check Firstname errors", async () => {
+  render(<App />);
+
+  const firstName = screen.getByTestId("firstName");
+
+  await act(async () => {
+    fireEvent.change(firstName, { target: { value: "Ismail" } });
+    fireEvent.blur(firstName);
+  });
+
+  expect(firstName).toBeInTheDocument();
+  expect(firstName).toHaveValue("Ismail");
+  expect(screen.queryByTestId("firstNameError")).not.toBeInTheDocument();
+});
+
+test("Check Lastname empty field", async () => {
+  render(<App />);
+
+  const lastName = screen.getByTestId("lastName");
+
+  await act(async () => {
+    fireEvent.change(lastName, { target: { value: "" } });
+    fireEvent.blur(lastName);
+  });
+
+  expect(lastName).toBeInTheDocument();
+  expect(lastName).toHaveValue("");
+  expect(screen.queryByTestId("lastNameError")).toBeInTheDocument();
+});
+
+test("Check Lastname errors", async () => {
+  render(<App />);
+
+  const lastName = screen.getByTestId("lastName");
+
+  await act(async () => {
+    fireEvent.change(lastName, { target: { value: "Al Kamal" } });
+    fireEvent.blur(lastName);
+  });
+
+  expect(lastName).toBeInTheDocument();
+  expect(lastName).toHaveValue("Al Kamal");
+  expect(screen.queryByTestId("lastNameError")).not.toBeInTheDocument();
+});
+
+test("Check Email empty field", async () => {
+  render(<App />);
+
+  const email = screen.getByTestId("email");
+
+  await act(async () => {
+    fireEvent.change(email, { target: { value: "" } });
+    fireEvent.blur(email);
+  });
+
+  expect(email).toBeInTheDocument();
+  expect(email).toHaveValue("");
+  expect(screen.queryByTestId("emailError")).toBeInTheDocument();
+});
+
+test("Check Correct Email errors", async () => {
+  render(<App />);
+
+  const email = screen.getByTestId("email");
+
+  await act(async () => {
+    fireEvent.change(email, { target: { value: "ismail@nawatt.com" } });
+    fireEvent.blur(email);
+  });
+
+  expect(email).toBeInTheDocument();
+  expect(email).toHaveValue("ismail@nawatt.com");
+  expect(screen.queryByTestId("emailError")).not.toBeInTheDocument();
+});
+
+test("Check Email format errors", async () => {
+  render(<App />);
+
+  const email = screen.getByTestId("email");
+
+  await act(async () => {
+    fireEvent.change(email, { target: { value: "Ismail" } });
+    fireEvent.blur(email);
+  });
+
+  expect(email).toBeInTheDocument();
+  expect(email).toHaveValue("Ismail");
+  expect(screen.queryByTestId("emailError")).toBeInTheDocument();
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,24 +16,31 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            data-testid="firstName"
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p>
+              Looks like there was an error:{" "}
+              <span data-testid="firstNameError">{errors.firstName.type}</span>
+            </p>
           )}
         </div>
-
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            data-testid="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
           />
           {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
+            <p>
+              Looks like there was an error:{" "}
+              <span data-testid="lastNameError">{errors.lastName.type}</span>
+            </p>
           )}
         </div>
 
@@ -41,14 +48,31 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input
+            data-testid="email"
+            name="email"
+            ref={register({
+              required: true,
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Enter a valid e-mail address",
+              },
+            })}
+          />
           {errors.email && (
-            <p>Looks like there was an error: {errors.email.type}</p>
+            <p>
+              Looks like there was an error:{" "}
+              <span data-testid="emailError">{errors.email.type}</span>
+            </p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            data-testid="message"
+            name="message"
+            ref={register({ required: false })}
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
