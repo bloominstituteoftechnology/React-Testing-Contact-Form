2d13757233fc5e9d86cd5a83584b5e541b19da1a
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-11-05 20:11:43 -0800

    Tests Working

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,37 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
+
 test("renders App without crashing", () => {
   render(<App />);
 });
 
 
-test('renders the app head', () =>{
-  render(<App />)
+test('user can fill out form', async () => {
+    render(<App />)
+
+const firstNameInput = screen.getByLabelText(/First Name/i);
+const lastNameInput = screen.getByLabelText(/Last Name/i);
+const emailInput = screen.getByLabelText(/email/i);
+
+fireEvent.change (firstNameInput, {target: {value: 'edd'}})
+fireEvent.change (lastNameInput, {target: {value: 'littlefield'}})
+fireEvent.change (emailInput, {target: {value: 'gmail'}})
+
+expect(firstNameInput).toHaveValue('edd')
+
+const button = screen.getByRole("button", {name: /submit/i })
+fireEvent.click(button)
+
+const newName = await screen.findByText(/edd/i);
+  expect(newName).toBeTruthy();
+  expect(newName).toHaveTextContent(/edd/);
+
+
+
 })
 
-
-
-
-
-
+// fireEvent.change(firstNameInput, {target: {value: 'edd'}})
 
 //Input fields work
 //submit button works
--- src/components/ContactForm.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {fireEvent, render, screen } from 'testing-library/react';
-
-import ContactForm from './ContactForm';
-
-
-test ('renders ContactForm', () => {
-    render(<ContactForm/>)
-})
