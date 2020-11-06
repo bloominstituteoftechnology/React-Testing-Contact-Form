be53fb0871f10a30650c4ca7f2c12c5a2f6d87e3
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-11-05 20:14:15 -0800

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -20,6 +20,8 @@ fireEvent.change (lastNameInput, {target: {value: 'littlefield'}})
 fireEvent.change (emailInput, {target: {value: 'gmail'}})
 
 expect(firstNameInput).toHaveValue('edd')
+expect(lastNameInput).toHaveValue('littlefield')
+expect(emailInput).toHaveValue('gmail')
 
 const button = screen.getByRole("button", {name: /submit/i })
 fireEvent.click(button)
@@ -28,7 +30,13 @@ const newName = await screen.findByText(/edd/i);
   expect(newName).toBeTruthy();
   expect(newName).toHaveTextContent(/edd/);
 
+  const newLastName = await screen.findByText(/littlefield/i);
+  expect(newLastName).toBeTruthy();
+  expect(newLastName).toHaveTextContent(/littlefield/);
 
+  const newEmail = await screen.findByText(/gmail/i);
+  expect(newEmail).toBeTruthy();
+  expect(newEmail).toHaveTextContent(/gmail/);
 
 })
 
