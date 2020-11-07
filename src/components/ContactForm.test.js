14b4bc7eb0e3ffd319dfbcc32469138eab64f201
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-11-07 08:09:45 -0800

    Completed Test

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,36 +2,38 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
-test('renders AnimalForm', () => {
+test('renders ContactForm', () => {
   render(<ContactForm />);
 })
 
-test('user can fill out and submit the form',  async() => {
+test('user can fill out and submit the form', async()  => {
     render(<ContactForm />);
-    const nameInput = screen.getByLabelText(/First Name/i)
-    const lastNameInput = screen.getByLabelText(/Last Name/i)
-    const emailInput = screen.getByLabelText(/email/i)
+    // Use aria-label instead of name for jsx label
+    const firstName = screen.getByLabelText(/firstName/i)
+    const lastName = screen.getByLabelText(/Last/i)
+    const email = screen.getByLabelText(/email/i)
+    const message = screen.getByLabelText(/message/i)
 
 
     fireEvent.change(firstName, { target:  {value: 'eddy'}})
     fireEvent.change(lastName, {target: {value: 'smooth'}});
     fireEvent.change(email, {target: {value: 'roofs@gmail.com'}});
+    fireEvent.change(message, { target:  {value: 'heres extra messages'}})
 
-
-    expect(nameInput).toHaveValue('eddy')
-
+    expect(firstName).toHaveValue('eddy')
+    expect(lastName).toHaveValue('smooth')
+    expect(email).toHaveValue('roofs@gmail.com')
+    expect(message).toHaveValue('heres extra messages')
 
     const button = screen.getByRole("button", { name: /submit/i });
     fireEvent.click(button);
-    // Sometimes in a form text may render before the form submits so you may need to run it async
-    // Async version must use await and have async declared up before the parenthesis uptop 
-    // const newAnimal = await screen.findByText(/dog/i)
-    // const newAnimal = await screen.findByText(/dog/i)
-    // expect(newAnimal).toHaveValue(/dog/i)
-    // expect(newAnimal).toBeTruthy();
-    // expect(newAnimal).toHaveTextContent(/dog/i)
+   
+    const aFirst = await screen.findAllByText(/First/i)
+    const aLast = await screen.findAllByText(/last name/i)
 
-    // These can use aria roles 
+    expect(aFirst).toBeTruthy();
+    expect(aLast).toBeTruthy();
+
 
 })
   
