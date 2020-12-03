9b1ebe94a9ee9262a8186f3cf6604b368395c070
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-03 17:00:47 -0500

    adds ids to inputs.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -28,6 +28,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,14 +42,22 @@ const ContactForm = () => {
           <label htmlFor="email">
             Email*
           </label>
-          <input name="email" placeholder="bluebill1049@hotmail.com" ref={register({ required: true })} />
+          <input name="email" 
+            id="lastName"
+            placeholder="bluebill1049@hotmail.com"
+            ref={register({ required: true })} 
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            name="message"
+            id="message" 
+            ref={register({ required: false })} 
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
