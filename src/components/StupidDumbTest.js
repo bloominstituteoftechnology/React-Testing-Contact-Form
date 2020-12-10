22f30967f09f0a0cab4974b301c1e4a4f47b4f7c
amotor-AM <66324211+amotor-AM@users.noreply.github.com>
2020-12-10 15:44:05 -0800

    Completed the project

--- src/components/StupidDumbTest.js
+++ src/components/StupidDumbTest.js
@@ -4,7 +4,7 @@ import { render, getByLabelText, getByType } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 import userEvent from "@testing-library/user-event";
 
-test("loads the component, fills out the form, and submits it. The same thing anyone can do manually by just spinning up an instance of the server but we have to write code to do the same thing we could very easily do much more efficiently because we are cool or something like that. I keep hoping that MAYBE we can get to a point were we are writing tests that are beneficial and do things more efficiently than just manually completing the action, however I am getting the impression that the entire reason testing exists is to force the developer to do more work so the managers or executives that want to test their feature do not have to submit any information manually. They just push a button or type a line of code and see everything working", async () => {
+test("loads the component, fills out the form, and submits it", async () => {
   const handleSubmit = jest.fn();
   render(<ContactForm />);
   const user = {
@@ -24,4 +24,3 @@ test("loads the component, fills out the form, and submits it. The same thing an
   expect(handleSubmit).toHaveBeenCalledWith(user);
 });
 
-// This test STILL DOES NOT RUN with npm test. I will take a fat 0 for this assignment. I don't even care. I ran npm start. i filled out the form. I saw the but myself. I fixed it. i wrote what SHOULD BE a working test. I spent too much time troubleshooting it. I looked up countless recources at this point. I have lost my will to care about this project anymore.
--- src/setupTests.js
+++ src/setupTests.js
@@ -8,9 +8,8 @@ import "mutationobserver-shim";
 
 import App from "./App";
 
-//apparently this test does not exist so I cant run it. This is WAAAAAAYYY faster than just manually typing the input into the component. I see why testing is important. You have to be able to build a component and then get frustrated as to why it is not testing the way it should be. Just the time I spent typing this comment is taking longer than manually testing the component. 
 
-// Find the fixed one in the components folder. The hour or so it will take me just to write a simple test is time well spent. I could have thuroughly manually tested the component in that time and probably created 1 -2 MORE components, but hey we gotta make sure a machine can also fill out our form. 
+// Find the fixed one in the components folder.
 
 test("Ensures that all labels mounted successfully", async () => {
   //Arrange
