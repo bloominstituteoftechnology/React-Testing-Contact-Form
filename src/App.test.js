046d93a6f2d0bc93e7a96a8cb4df38aa28a83ad8
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-12-11 13:42:16 -0500

    created App.test.js and added a simple test

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('App mounts', () => {
+	//Arrange
+	render(<App />);
+
+	//Act
+
+	//Assert
+});
