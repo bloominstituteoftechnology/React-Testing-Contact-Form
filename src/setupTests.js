07a2ecddc2c0db976a70504147082bfbb0f9eb7e
RubesWorld <hi@rubesworld.live>
2021-01-14 16:28:39 -0800

    Completed MVP

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,9 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+//component import
+import App from './App'
+
