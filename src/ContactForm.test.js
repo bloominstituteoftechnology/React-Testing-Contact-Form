bb7b14fb27e047f17210b1940665ca3e01c13881
Jacob Stephens <jacobstephens2@gmail.com>
2021-01-14 16:44:11 -0800

    finished most

--- src/ContactForm.test.js
+++ src/ContactForm.test.js
@@ -4,19 +4,49 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './components/ContactForm'
 
 test("renders without errors", () => {
-render(<ContactForm/>)
+    render(<ContactForm/>)
+});
 
-const firstName = screen.getByPlaceholderText(/edd/i);
-const lastName = screen.getByPlaceholderText(/burke/i);
-const email = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+test("testing first name", () => {
 
-userEvent.type(firstName, "Jacob")
-userEvent.type(lastName, "Stephens")
-userEvent.type(email, "jacobstephens.work@gmail.com")
+    render(<ContactForm/>)
 
+    const firstName = screen.getByPlaceholderText(/Edd/i);
 
-const button = screen.getByRole('button')
-userEvent.click(button)
+    userEvent.type(firstName, "Jacob")
 
+    expect(firstName).toBeTruthy();
+    expect(firstName).toBeInTheDocument();
+    expect(firstName).toHaveTextContent('');
 
 });
+
+test("testing last name", () => {
+    render(<ContactForm/>)
+    const lastName = screen.getByPlaceholderText(/burke/i);
+    userEvent.type(lastName, "Stephens")
+    
+    expect(lastName).toBeTruthy();
+    expect(lastName).toBeInTheDocument();
+    expect(lastName).toHaveTextContent('');
+
+});
+
+test("testing email",() => {
+    render(<ContactForm/>)
+    const email = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+    userEvent.type(email, "jacobstephens.work@gmail.com")
+
+    expect(email).toBeTruthy();
+    expect(email).toBeInTheDocument();
+    expect(email).toHaveTextContent('');
+});
+
+test("testing button", () => {
+    render(<ContactForm/>)
+    const button = screen.getByRole('button')
+    userEvent.click(button)
+
+    expect(button).toBeInTheDocument();
+});
+
