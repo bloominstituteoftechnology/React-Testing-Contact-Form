4d0b69cfa8af5a696648cfc52603c2a989f026af
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-01-21 23:18:27 -0800

    Testing Contact Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
 import ContactForm from "./components/ContactForm";
 
 export default function App() {
+  
   return (
     <div className="App">
+      
       <ContactForm />
     </div>
   );
