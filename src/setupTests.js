5f69faafa6b3b80554acb06f63c5e62e7c73deab
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-02-11 15:04:22 -0500

    testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,15 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ContactForm from './components/ContactForm';
+
+test('', () => {
+    // render the component I want to test
+    render(<ContactForm />);
+
+    // query for each input and assign to a variable
+    const firstNameInput = screen.queryByLabelText(/firstname/i);
+});
