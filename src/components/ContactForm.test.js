db5f1c4f3ef1ff202a2303db8a3bc5f6acd0636c
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-02-11 18:11:57 -0500

    act/async now working

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -4,26 +4,29 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 import { act } from 'react-dom/test-utils';
 
-test('test to see if forms filled out and button press, object is created', () => {
+test('test to see if forms filled out and button press, object is created', async () => {
     // render the component I want to test
    render(<ContactForm />);
 
     // query for each input and assign to a variable
-    const firstNameInput = screen.getByPlaceholderText(/edd/i);
-    const lastNameInput = screen.getByPlaceholderText(/burke/i);
-    const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
-    const messageInput = screen.queryByLabelText(/message/i);
-    // query for the button
-    const submitButton = screen.getByRole('button', {name: /submit/i});
+    await act(async () => {
+        const firstNameInput = screen.getByPlaceholderText(/edd/i);
+        const lastNameInput = screen.getByPlaceholderText(/burke/i);
+        const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+        const messageInput = screen.queryByLabelText(/message/i);
+        // query for the button
+        const submitButton = screen.getByRole('button', {name: /submit/i});
 
-    // type into input fields
-    userEvent.type(firstNameInput, 'Test');
-    userEvent.type(lastNameInput, 'Person');
-    userEvent.type(emailInput, 'test@test.com');
-    userEvent.type(messageInput, 'this is a message');
-    
-    //click the button
-    userEvent.click(submitButton);
+        // type into input fields
+        userEvent.type(firstNameInput, 'Test');
+        userEvent.type(lastNameInput, 'Person');
+        userEvent.type(emailInput, 'test@test.com');
+        userEvent.type(messageInput, 'this is a message');
+        
+        //click the button
+        
+        userEvent.click(submitButton);
+     });
 
     // assertion
     expect.objectContaining({
