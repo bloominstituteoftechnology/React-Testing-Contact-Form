7e7cfa3636663f1849de08565c46cd0c9f777136
hdm007 <harry.mardirossian13@gmail.com>
2021-02-11 23:15:33 -0500

    bug fixed, id and htmlFor did not match on form labels

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,4 +6,9 @@ import ContactForm from "./ContactForm"
 
 test("testing form", () => {
     render(<ContactForm />)
+
+    const firstName = screen.getByLabelText((/first name/i));
+    const lastName=screen.getByLabelText((/last name/i));
+    const email = screen.getByLabelText(/email/i);
+    const message = screen.getByLabelText(/message/i);
 })
