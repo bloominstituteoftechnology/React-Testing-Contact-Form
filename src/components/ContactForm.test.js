22f3a376e02521a5a5f46b97500c4d30e7e6119d
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-02-18 21:14:07 -0500

    added some more tests

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, screen } from  "@testing-library/react";
+import { findByText, render, screen } from  "@testing-library/react";
 import ContactForm from "./ContactForm";
+import  userEvent  from "@testing-library/user-event";
 
 test('renders ContactForm without errors ', ()=>{
     render(<ContactForm />)
@@ -8,11 +9,17 @@ test('renders ContactForm without errors ', ()=>{
 
 test('the user can fill out all portions of the form and submit', async()=>{
     render(<ContactForm />)
-    const firstNameInput = screen.getByLabelText(/firstName/i);
+    // const firstNameInput = screen.getByLabelText(/firstName/i);
     const lastNameInput = screen.getByLabelText(/lastName/i);
-    const emailInput = screen.getByLabelText(/name/i);
-    const messageInput = screen.getByLabelText(/name/i);
-    const nameInput = screen.getByLabelText(/name/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const messageInput = screen.getByLabelText(/message/i);
+    const submitButton = screen.getByRole('submit', {name: /submit/i});
 
+    // userEvent.type(firstNameInput, "Joe");
+    userEvent.type(lastNameInput, "Baker");
+    userEvent.type(emailInput, "JB123@email.com");
+    userEvent.type(messageInput, "Hello World")
+    userEvent.click(submitButton)
 
+    const newUser = findByText
 })
