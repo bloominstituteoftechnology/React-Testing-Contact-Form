c12b6299410678d4c9bc35f3bc90af7f778f3067
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-18 18:48:34 -1000

    MVP complete

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+
+
 import 'mutationobserver-shim';
+import App from './App'
+
+
+
+
+
+
